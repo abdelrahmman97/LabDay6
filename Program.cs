6e972a430f68a12bbdcb15dd4b942bf1a3077365
Manar <eng.manar666@gmail.com>
2023-09-25 15:18:00 +0300

    Program.exe

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stock stock = new Stock();
+            stock.Price = 100;
+
+            stock.OnPriceChanged += Stock_OnPriceChanged;
+            stock.ChangeStockPriceByPercent(0.05m);
+            stock.ChangeStockPriceByPercent(-0.02m);
+            stock.ChangeStockPriceByPercent(0.00m);
+
+        }
+
+        static void Stock_OnPriceChanged(Stock stock, decimal oldPrice)
+        {
+            if (stock.Price > oldPrice) { Console.WriteLine($"new price : {stock.Price}"); }
+            if (stock.Price < oldPrice) { Console.WriteLine($"new price : {stock.Price}"); }
+            if (stock.Price == oldPrice) { Console.WriteLine($"new price : {stock.Price}"); }
+        }
+    }
+}
---
+++ StockSubscriber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day6
+{
+    public class StockSubscriber
+    {
+        public StockSubscriber(Stock stock)
+        {
+            stock.OnPriceChanged += HandlePriceChanged;
+        }
+
+        public void HandlePriceChanged(Stock stock, decimal oldPrice)
+        {
+            Console.WriteLine($"Old price: {oldPrice}, New price: {stock.Price}");
+        }
+    }
+}
