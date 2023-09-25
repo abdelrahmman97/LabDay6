575139216a6c6391541247c4eb3ac0b121f55329
abdelrahmman97 <mabdelrahmman22@gmail.com>
2023-09-25 12:13:28 +0200

    Add project files.

---
+++ Stock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day6
+{
+    public delegate void StockPriceChagedHandler(Stock stock, decimal oldPrice);
+
+    public class Stock
+    {
+        private decimal price;
+        public decimal Price { get; set; }
+
+        public event StockPriceChagedHandler OnPriceChanged;
+
+        public void ChangeStockPriceByPercent(decimal percent)
+        {
+            decimal oldPrice = this.price;
+            this.price += Math.Round(this.price * percent, 2);
+
+            if (OnPriceChanged != null)
+            {
+                OnPriceChanged(this, oldPrice);
+            }
+        }
+
+    }
+}
